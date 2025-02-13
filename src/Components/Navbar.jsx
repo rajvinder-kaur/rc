c1da2b532e7a1a258ccbcba45eb21ae97ffc9cb3
rajvinder-kaur <kaurrajvinder736@gmail.com>
2025-02-13 21:57:28 +0530

    stable navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router';
-
 
 function Navbar() {
   return (
-    <div className="navbar bg-transparent backdrop-blur-sm shadow-sm ">
+    <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -13,33 +11,39 @@ function Navbar() {
       <ul
         tabIndex={0}
         class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><Link to='/'>Why Us?</Link></li>
+        <li><a>Item 1</a></li>
         <li>
-          <Link to ='/'>Turnkey</Link>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
         </li>
-        <li><Link to ='/'>Collaboration</Link></li>
-        <li><Link to ='/'>Our Projects</Link></li>
+        <li><a>Item 3</a></li>
       </ul>
     </div>
-    <Link to ='/' className='font-bold'>RC constructions</Link>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-    <li><Link to='/'>Why Us?</Link></li>
-        <li>
-          <Link to ='/'>Turnkey</Link>
-        </li>
-        <li><Link to ='/'>Collaboration</Link></li>
-        <li><Link to ='/'>Our Projects</Link></li>
+      <li><a>Item 1</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Item 3</a></li>
     </ul>
   </div>
   <div className="navbar-end">
-  <button className=' bg-[#D1683D] p-3 rounded text-white'><Link to='/' >Contact Us</Link></button>
+    <a className="btn">Button</a>
   </div>
 </div>
   )
 }
 
-export default Navbar;
-
-
+export default Navbar
